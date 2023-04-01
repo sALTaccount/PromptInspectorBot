be2d9172dea414c8d8ade5676990145e3fc6622d
NoCrypt <57245077+NoCrypt@users.noreply.github.com>
2023-04-01 13:26:04 +0700

    Revive PI
    Only work for images that generated with https://github.com/ashen-sensored/sd_webui_stealth_pnginfo

--- PromptInspector.py
+++ PromptInspector.py
@@ -49,6 +49,64 @@ def get_embed(embed_dict, context):
     return embed
 
 
+def read_info_from_image_stealth(image):
+    # trying to read stealth pnginfo
+    width, height = image.size
+    pixels = image.load()
+
+    binary_data = ''
+    buffer = ''
+    index = 0
+    sig_confirmed = False
+    confirming_signature = True
+    reading_param_len = False
+    reading_param = False
+    read_end = False
+    for x in range(width):
+        for y in range(height):
+            _, _, _, a = pixels[x, y]
+            buffer += str(a & 1)
+            if confirming_signature:
+                if index == len('stealth_pnginfo') * 8 - 1:
+                    if buffer == ''.join(format(byte, '08b') for byte in 'stealth_pnginfo'.encode('utf-8')):
+                        confirming_signature = False
+                        sig_confirmed = True
+                        reading_param_len = True
+                        buffer = ''
+                        index = 0
+                    else:
+                        read_end = True
+                        break
+            elif reading_param_len:
+                if index == 32:
+                    param_len = int(buffer, 2)
+                    reading_param_len = False
+                    reading_param = True
+                    buffer = ''
+                    index = 0
+            elif reading_param:
+                if index == param_len:
+                    binary_data = buffer
+                    read_end = True
+                    break
+            else:
+                # impossible
+                read_end = True
+                break
+
+            index += 1
+        if read_end:
+            break
+
+    if sig_confirmed and binary_data != '':
+        # Convert binary string to UTF-8 encoded text
+        decoded_data = bytearray(int(binary_data[i:i + 8], 2) for i in range(0, len(binary_data), 8)).decode('utf-8',errors='ignore')
+
+        geninfo = decoded_data
+
+    return geninfo
+
+
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}!")
@@ -62,10 +120,10 @@ async def on_message(message):
                 image_data = await attachment.read()
                 with Image.open(io.BytesIO(image_data)) as img:
                     try:
-                        metadata = img.info
-                        metadata = metadata['parameters']
-                        get_embed(get_params_from_string(metadata), message)
-                        await message.add_reaction('🔎')
+                        metadata = read_info_from_image_stealth(img)
+                        if 'Steps: ' in metadata:
+                          get_embed(get_params_from_string(metadata), message)
+                          await message.add_reaction('🔎')
                     except:
                         pass
 
@@ -99,8 +157,7 @@ async def on_raw_reaction_add(ctx):
                     image_data = await attachment.read()
                     with Image.open(io.BytesIO(image_data)) as img:
                         try:
-                            metadata = img.info
-                            metadata = metadata['parameters']
+                            metadata = read_info_from_image_stealth(img)
                             embed = get_embed(get_params_from_string(metadata), message)
                             embed.set_image(url=attachment.url)
                             user_dm = await client.get_user(ctx.user_id).create_dm()
